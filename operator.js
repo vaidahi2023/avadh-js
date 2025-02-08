b31c2949ae5b48e2914bcec9c8c0a30598045c71
vaidahi2023 <vaidahi810@gmail.com>
2025-02-08 19:25:03 +0530

    operators

---
+++ operator.js
@@ -0,0 +1,34 @@
+var a=10,b="10"
+let c=20
+
+// operators 
+      // //arthimetic operators--+,-,/,*,%
+    //   console.log("a+b="+(a+b))
+    //   console.log("a-b="+(a-b))
+    //   console.log("a*b="+(a*b))
+    //   console.log("a/b="+(a/b))
+    //   console.log("a%b="+(a%b))
+
+    //    //assignmment operators-- +=,-=,/=,*=,**
+    //   a=a+1
+    //   a+=1
+    //   console.log(a)
+    //   console.log(2**3)
+       //comparison operators -- >,<,>=,<=,==,===,!=,!==
+       //== --it will check only value 
+    //   console.log(a==b)
+     //=== --it will check values and datatype also 
+    //   console.log(a===b)
+    //   console.log(c!=b)
+    //   console.log(c!==b)
+
+       // //logical operators-- &&,||,!
+       //&& --and
+       //|| --or 
+       //!--not
+      console.log((a==b)&&(a!=c))
+      console.log((a==b)&&(a==c))
+      console.log((a==b)||(a==c))
+      console.log(!(a==b))
+      console.log(!(a!=c))
+    
